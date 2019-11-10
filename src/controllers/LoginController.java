4d0c6960e07ec90e6488e741a90c0514e18b0d70
Fakultet Organizacionih Nauka <student1@prod.fon.bg.ac.rs>
2019-11-10 02:08:05 +0100

    Promena sa prve na drugu scenu

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -1,4 +1,13 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import services.StageService;
+
 public class LoginController {
+
+    public void changeScene2(ActionEvent event){
+        StageService stageService = new StageService();
+        stageService.changeScene("resources/view/loginScene.fxml", event);
+
+    }
 }
---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void metoda(ActionEvent e){
+        System.out.println("123");
+    }
+}
